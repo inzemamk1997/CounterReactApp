ba6e6fd7ac80feb89b50e91259a5ee31b501a511
Inzemam Khan <inzemamk1997@gmail.com>
2020-12-01 16:53:29 +0530

    Counter for a specific range

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => {
           Three-State-Counter
           </h1>
           <h2>The current state counter is {count}</h2>
-          <button onClick = {()=>setCount(count+1)}>Increase Count</button>
-          <button onClick = {()=>setCount(count-1)}>Decrease Count</button>
+          <button onClick = {()=> (count>=10) ? "": setCount(count+1)}>Increase Count</button>
+          <button onClick = {()=> (count<=0) ?"" : setCount(count-1)}>Decrease Count</button>
           <button onClick = {()=>setCount(0)}>Reset Count </button>
         </div>
 );
